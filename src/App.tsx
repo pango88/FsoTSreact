93d5e5a114e14f591b559f02268606feac8751b1
pango88 <hansflame@protonmail.com>
2020-09-15 00:18:38 +0200

    finished 9.15

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,31 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 import Total from './Total';
+import { CoursePart } from './types';
 
 const App: React.FC = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
+      description: 'This is an awesome course part',
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
+      groupProjectCount: 3,
     },
     {
       name: 'Deeper type usage',
       exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+    },
+    {
+      name: 'Assert error never',
+      exerciseCount: 3,
+      description: 'Unsure about this',
     },
   ];
 
--- src/Content.tsx
+++ src/Content.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
-import { CoursePart } from './types';
+import { CoursePart, assertNever } from './types';
 
 const Content: React.FC<{ courseParts: Array<CoursePart> }> = ({
   courseParts,
 }) => {
   return (
     <div>
-      {courseParts.map((c) => (
-        <p key={c.name}>
-          {c.name} {c.exerciseCount}
-        </p>
-      ))}
+      {courseParts.map((part) => {
+        switch (part.name) {
+          case 'Fundamentals':
+            return (
+              <div>
+                {part.name} {part.exerciseCount} {part.description}
+              </div>
+            );
+          case 'Using props to pass data':
+            return (
+              <div>
+                {part.name} {part.exerciseCount} {part.groupProjectCount}
+              </div>
+            );
+          case 'Deeper type usage':
+            return (
+              <div>
+                {part.name} {part.exerciseCount} {part.description}{' '}
+                {part.exerciseSubmissionLink}
+              </div>
+            );
+          case 'Assert error never':
+            return (
+              <div>
+                {part.name} {part.exerciseCount} {part.description}{' '}
+              </div>
+            );
+          default:
+            return assertNever(part);
+        }
+      })}
     </div>
   );
 };
